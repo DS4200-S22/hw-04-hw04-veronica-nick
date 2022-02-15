d15817f05bddc2403737ef8e1431a7b209c6a0a0
nick-coury <nick.coury@sbcglobal.net>
2022-02-15 13:38:14 -0500

    make circle click counter

--- hw4.js
+++ hw4.js
@@ -1,1 +1,7 @@
-hw4.js
+let numC1Click = 0;
+
+function circleClicked() { 
+    let circleSVG = document.getElementById("circleText"); 
+    circleSVG.innerHTML = numC1Click + 1; 
+    numC1Click++;
+}
