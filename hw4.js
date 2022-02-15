c8fb5975d4e6314ebeaf3b5b5a08753809b7507f
nick-coury <nick.coury@sbcglobal.net>
2022-02-15 17:34:15 -0500

    make third circle function

--- hw4.js
+++ hw4.js
@@ -5,3 +5,10 @@ function circle1Clicked() {
     circleSVG.innerHTML = numC1Click + 1; 
     numC1Click++;
 }
+
+function circle3Clicked() { 
+    let circleSVG = document.getElementById("c3"); 
+    
+    circleSVG.style.opacity = "0.2"; 
+    
+}
