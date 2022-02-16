39bc11372cf7a21dca2cfa757884fbcdde230a09
veroaaguiar <aguiarortega.ve@husky.neu.edu>
2022-02-16 12:30:21 -0500

    final commit

--- hw4.js
+++ hw4.js
@@ -7,13 +7,7 @@ function circle1Clicked() {
 }
 
 function circle2Hovered() { 
-<<<<<<< HEAD
-    let circleSVG = document.getElementById("c2"); 
-    circleSVG.onhover = (event) => {
-      //change to make the border change instead of console message
-    console.log('hovered!');
-  };
-=======
+
   let circleSVG = document.getElementById("c2"); 
   if(onmousover){
     circleSVG.style.border === "5px solid black"
@@ -21,7 +15,6 @@ function circle2Hovered() {
   if(onmouseout){
     circleSVG.style.border = "";
   }
->>>>>>> 93ee0538f4e419a52d7fcf10ae123b1c6ab3154a
 }
 
 
